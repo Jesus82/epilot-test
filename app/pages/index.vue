e5dbc7a613b273f9b81707d4f5f3534dcbdbaf51
joe82 <ourensan@gmail.com>
2025-12-01 16:46:31 +0100

    added price to html

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { formattedPrice, status, connect, disconnect } = useBtcPrice()
+
+onMounted(() => connect())
+onUnmounted(() => disconnect())
+</script>
+
+<template>
+  <main>
+    <h1>BTC Price</h1>
+    <p>Status: {{ status }}</p>
+    <p>{{ formattedPrice ?? 'Loading...' }}</p>
+  </main>
+</template>
