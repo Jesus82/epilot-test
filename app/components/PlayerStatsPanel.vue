add5ae7a37487fd7814f0e121567cc127c1530ed
joe82 <ourensan@gmail.com>
2025-12-09 12:39:51 +0100

    ui fix

--- app/components/PlayerStatsPanel.vue
+++ app/components/PlayerStatsPanel.vue
@@ -14,48 +14,48 @@ const { formatPrice } = useBtcPrice()
 </script>
 
 <template>
-  <div>
-    <div>
+  <ul>
+    <li>
+      <p class="font-display text-md">
+        Current Streak:
+        <span>{{ currentStreak }}</span>
+      </p>
+    </li>
+    <li>
+      <p class="font-display text-md">
+        Longest Streak:
+        <span>{{ longestStreak }}</span>
+      </p>
+    </li>
+    <li>
+      <p class="font-display text-md">
+        Wins:
+        <span>{{ totalWins }}</span>
+      </p>
+    </li>
+    <li>
+      <p class="font-display text-md">
+        Losses:
+        <span>{{ totalLosses }}</span>
+      </p>
+    </li>
+    <li>
       <p class="font-display text-md">
         Total Earnings:
         <span :class="totalEarnings >= 0 ? 'text-green' : 'text-red'">
           {{ totalEarnings >= 0 ? '+' : '' }}{{ formatPrice(totalEarnings) }}
         </span>
       </p>
-    </div>
-    <div>
-      <p class="font-display text-md">
-        Current Streak:
-        <span>{{ currentStreak }}</span>
-      </p>
-    </div>
-    <div>
-      <p class="font-display text-md">
-        Longest Streak:
-        <span>{{ longestStreak }}</span>
-      </p>
-    </div>
-    <div>
-      <p class="font-display text-md">
-        Wins:
-        <span>{{ totalWins }}</span>
-      </p>
-    </div>
-    <div>
-      <p class="font-display text-md">
-        Losses:
-        <span>{{ totalLosses }}</span>
-      </p>
-    </div>
-    <div v-if="isLocked">
+    </li>
+    <li v-if="isLocked">
       <p class="font-display text-md">
         Potential Earnings:
         <span :class="potentialEarnings >= 0 ? 'text-green' : 'text-red'">
           {{ potentialEarnings >= 0 ? '+' : '' }}{{ formatPrice(potentialEarnings) }}
         </span>
       </p>
-    </div>
-    <div v-if="lastBidResult">
+    </li>
+    <li v-if="lastBidResult">
       <p class="font-display text-md">
         Last Bid:
         <span :class="lastBidResult.won ? 'text-green' : 'text-red'">
@@ -63,6 +63,6 @@ const { formatPrice } = useBtcPrice()
           {{ lastBidResult.earnings >= 0 ? '+' : '' }}{{ formatPrice(lastBidResult.earnings) }}
         </span>
       </p>
-    </div>
-  </div>
+    </li>
+  </ul>
 </template>
